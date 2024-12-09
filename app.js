ef25245ad4b2f4e2c8072966e624f2de0229f0cb
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-12-09 16:28:42 +0300

    ejs

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views'); 
+
+app.use(express.static('public'));
+
 
 const blog = { id: 1, title: "Blog title", description: "Blog description" };
 
